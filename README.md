a53b5e2fb8a78995565f287dd4aa864282b9c784
Yan <yanpgabriel@gmail.com>
2021-05-01 16:52:59 -0300

    ~ Commit Inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+![Logo Quarkus](https://quarkus.io/assets/images/quarkus_logo_horizontal_rgb_reverse.svg)
+# Projeto DuckAPI 
+###### powered by quarkus
+
+---
---
+++ src/main/java/dev/yanpgabriel/duck/DuckAPI.java
@@ -0,0 +1,8 @@
+package dev.yanpgabriel.duck;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class DuckAPI extends Application {
+}
