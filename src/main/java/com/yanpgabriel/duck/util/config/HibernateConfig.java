15ba9970dafbd412f89c790e4233a77a1f3c7923
Yan P. Gabriel <yanpgabriel@gmail.com>
2023-08-10 22:29:37 -0300

    ~ Definindo valor padrão para as configs do hibernate se não setado

--- src/main/java/com/yanpgabriel/duck/util/config/HibernateConfig.java
+++ src/main/java/com/yanpgabriel/duck/util/config/HibernateConfig.java
@@ -6,10 +6,12 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Singleton
 public class HibernateConfig {
 
-    @ConfigProperty(name = "quarkus.hibernate-orm.sql-load-script")
+    private static final String NAO_DEFINIDO = "Não definido para esse profile";
+
+    @ConfigProperty(name = "quarkus.hibernate-orm.sql-load-script", defaultValue = NAO_DEFINIDO)
     public String sqlLoadScript;
 
-    @ConfigProperty(name = "quarkus.hibernate-orm.database.generation")
+    @ConfigProperty(name = "quarkus.hibernate-orm.database.generation", defaultValue = NAO_DEFINIDO)
     public String databaseGeneration;
 
 }
